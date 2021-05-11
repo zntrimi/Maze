c85d640a7c37521337dbde8df808118f107a6c17
Zentaro Imai <zentaro0824@gmail.com>
2021-05-11 16:36:01 +0900

    Initial Commit

---
+++ Maze/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Maze
+//
+//  Created by Zentaro Imai on 2021/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
